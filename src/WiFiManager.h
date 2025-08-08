a48d314eb3a79204a363cce61f143220f2f2e424
Richard Beech <rp.beech@gmail.com>
2025-08-09 00:46:37 +0100

    Use single web server instance

--- src/WiFiManager.h
+++ src/WiFiManager.h
@@ -1,5 +1,11 @@
+// WiFiManager.h
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
+
+#include <ESPAsyncWebServer.h>
+
+// Global web server used by both the WiFi config portal and the camera UI
+extern AsyncWebServer server;
 
 // Connect to WiFi using credentials saved in preferences.
 bool connectToStoredWiFi();
